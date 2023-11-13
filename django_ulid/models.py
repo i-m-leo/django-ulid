43fc62a7003481af45e4aed010e280156433fdcf
Leo <7888621+i-m-leo@users.noreply.github.com>
2023-11-13 18:15:57 +0800

    Fix some bugs found in the issues of the origin repo.

--- django_ulid/models.py
+++ django_ulid/models.py
@@ -6,13 +6,14 @@
 """
 import ulid
 from django.core import exceptions
-from django.db import models
+from django.db import models, connection as db_conn
 from django.utils.translation import gettext as _
 
 from . import forms
 
-# Helper attr so callers don't need to import the ulid package.
-default = ulid.new
+# Helper function so callers don't need to import the ulid package.
+def default():
+    return ulid.new()
 
 
 class ULIDField(models.Field):
@@ -34,7 +35,7 @@ class ULIDField(models.Field):
         return name, path, args, kwargs
 
     def get_internal_type(self):
-        return 'UUIDField'
+        return 'UUIDField' if db_conn.features.has_native_uuid_field else 'CharField'
 
     def get_db_prep_value(self, value, connection, prepared=False):
         if value is None:
